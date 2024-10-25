5b1dd6961e0ad84d274e7849065b2d62a8f79aba
minion-star <williamalderette799@gmail.com>
2024-10-25 08:08:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,152 +1,139 @@
-//File name is App.js
-//It contains the form, its structure
-//and Basic Form Functionalities
-
+import Board from './components/Board'
 import './App.css';
-import {react, useState} from 'react';
+import React, { useState , createContext, useEffect } from 'react';
+import Header from './components/Header';
+
+export const AppContext = createContext();
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [contact, setContact] = useState("");
-  const [gender, setGender] = useState("male");
-  const [subejcts,setSubjects] = useState({
-    english: true,
-    maths: false,
-    physics: false,
-  });
-  const [resume, setResume] = useState("");
-  const [url, setUrl] = useState();
-  const [selectedOption, setSelectedOption] = useState("");
-  const [about, setAbout] = useState("");
+  const emptyGame = [["", "", ""], ["", "", ""], ["", "", ""]]
+  const [cells, setCells] = useState(emptyGame)
+  const [winnerCells, setWinnerCells] = useState([[],[],[]])
 
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    console.log(
-      firstName,
-      lastName,
-      email,
-      contact,
-      gender,
-      selectedOption,
-      subejcts,
-      resume,
-      url,
-      about
-    );
-    //Add your form submission logic here
-  };
+  const X = "X"
+  const O = "O"
+  const [currentChar, setCurrentChar] = useState(X);
+  const [winner, setWinner] = useState("");
+  const [gameOver, setGameOver] = useState(false)
 
-  const handleSubjectChange = (sub)=>{
-    setSubjects((prev)=>({
-      ...prev, [sub]: !prev[sub],
-    }));
-  };
+  useEffect(function() {
+    isGameOver()
+  }, [cells])
 
-  const handleReset=()=>{
-    //Reset all state variables here
-    setFirstName("");
-    setLastName("");
-    setEmail("");
-    setContact("");
-    setGender("");
-    setSubjects({
-      english: true,
-      maths: false,
-      physics: false,
-    });
-    setResume("");
-    setUrl("");
-    setSelectedOption("");
-    setAbout("");
-  };
+  function cellClick(row, column) {
+    if (gameOver || winner) {
+      return;
+    }
 
-  return(
-    <div className='App'>
-      <h1>Form in React</h1>
-      <fieldset>
-        <form action="#" method="get">
-          <label for="firstname">First Name*</label>
-          <input
-            type='text'
-            name='firstname'
-            id='firstname'
-            value={firstName}
-            onChange={(e)=>setFirstName(e.target.value)}
-            placeholder='Enter First Name'
-            required 
-          />
-          <label for="lastname">Last Name*</label>
-          <input
-            type='text'
-            name='lastname'
-            id='lastname'
-            value={lastName}
-            onChange={(e)=>setLastName(e.target.value)}
-            placeholder='Enter Last Name'
-            required
-          />
-          <label for="email">Enter Email*</label>
-          <input
-            type='email'
-            name='email'
-            id='email'
-            value={email}
-            onChange={(e)=>setEmail(e.target.value)}
-            placeholder='Enter email'
-            required
-          />
-          <label for="tel">Contact*</label>
-          <input
-            type='tel'
-            name='contact'
-            id='contact'
-            value={contact}
-            onChange={(e)=>setContact(e.target.value)}
-            placeholder='Enter Mobile number'
-            required
-          />
-          <label for="gender">Gender*</label>
-          <input
-            type='radio'
-            name='gender'
-            value='male'
-            id='male'
-            checked={gender==="male"}
-            onChange={(e)=>setGender(e.target.value)}
-          />
-          Male
-          <input
-            type='radio'
-            name='gender'
-            value='female'
-            id='female'
-            checked={gender==="female"}
-            onChange={(e)=>setGender(e.target.value)}
+    if (cells[row][column] != "") {
+      return;
+    }
 
-          />
-          Female
-          <input
-            type='radio'
-            name='gender'
-            value='other'
-            id='other'
-            checked={gender==="other"}
-            onChange={(e)=>setGender(e.target.value)}
+    const newBoard = {...cells}
+    newBoard[row][column] = currentChar
+    setCells(newBoard)
+    changeChar()
+  }
 
-          />
-          other
-          <label for="lang">Your best Subject</label>
-          <input
-            type='checkbox'
-            name='lang'
-            id='english'
-            checked={subejcts.english===true}
-            onChange={(e)=>handleSubjectChange("english")}
-          />
-        </form>
-      </fieldset>
+  function changeChar() {
+    if (currentChar == X) {
+      setCurrentChar(O)
+    } else {
+      setCurrentChar(X)
+    }
+  }
+
+  function reset() {
+    setCells(emptyGame)
+    setWinner("")
+    setGameOver(false)
+    setWinnerCells([[],[],[]])
+  }
+
+  function isGameOver() {
+    switch (true) {
+      case areTheSameInRow(0): return
+      case areTheSameInRow(1): return
+      case areTheSameInRow(2): return
+      case areTheSameInColumn(0): return
+      case areTheSameInColumn(1): return
+      case areTheSameInColumn(2): return
+      case areTheSameInDiagonal(): return
+    }
+
+    if (!cells[0].includes("") && !cells[1].includes("") && !cells[2].includes("")) {
+      endGame("")
+    }
+  }
+
+  function endGame(winner) {
+    if (winner != "") setWinner(winner)
+    setGameOver(true)
+  }
+
+  function areTheSameInRow(row) {
+    if (cells[row][0] !== "" && cells[row][0] === cells[row][1] && cells[row][0] === cells[row][2]) {
+      endGame(cells[row][0])
+      
+      const newWinner = [[],[],[]]
+      newWinner[row][0] = true
+      newWinner[row][1] = true
+      newWinner[row][2] = true
+      setWinnerCells(newWinner)
+      return true
+    }
+    return false
+  }
+
+  function areTheSameInColumn(column) {
+    if (cells[0][column] !== "" && cells[0][column] === cells[1][column] && cells[2][column] == cells[0][column]) {
+      endGame(cells[0][column])
+      
+      const newWinner = [[],[],[]]
+      newWinner[0][column] = true
+      newWinner[1][column] = true
+      newWinner[2][column] = true
+      setWinnerCells(newWinner)
+      return true
+    }
+    return false
+  }
+
+  function areTheSameInDiagonal() {
+    if (cells[1][1] != "" && (cells[0][0] === cells[1][1] && cells[0][0] == cells[2][2])) {
+      endGame(cells[1][1])
+      const newWinner = [[],[],[]]
+      newWinner[0][0] = true
+      newWinner[1][1] = true
+      newWinner[2][2] = true
+      setWinnerCells(newWinner)
+      return true
+    }
+
+    if (cells[1][1] != "" && (cells[2][0] === cells[1][1] && cells[2][0] == cells[0][2])) {
+      endGame(cells[1][1])
+      const newWinner = [[],[],[]]
+      newWinner[0][2] = true
+      newWinner[1][1] = true
+      newWinner[2][0] = true
+      setWinnerCells(newWinner)
+      return true
+    }
+    return false
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AppContext.Provider value={{cells, setCells, cellClick, currentChar, winner, gameOver, winnerCells}}>
+          <Header />
+          <Board />
+        </AppContext.Provider>
+
+        <button className='btn-reset' onClick={() => reset()}>Reset</button>
+      </header>
     </div>
   );
 }
+
+export default App;
